92d036a5e184ba6b0af7972aaf012ee3fc27cb54
Prasanna V K <prasannavkp@gmail.com>
2020-08-11 17:17:13 +0530

    Users and Groups/Role

--- TRMDataManager/Controllers/UserController.cs
+++ TRMDataManager/Controllers/UserController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -6,6 +7,7 @@ using System.Web;
 using System.Web.Http;
 using TRMDataManager.Library.DataAccess;
 using TRMDataManager.Library.Models;
+using TRMDataManager.Models;
 
 namespace TRMDataManager.Controllers
 {
@@ -23,5 +25,52 @@ namespace TRMDataManager.Controllers
       return data.GetUserById(userId).First();
     }
 
+
+    [Authorize(Roles = "Admin")]
+    [HttpGet]
+    [Route("api/User/Admin/GetAllUsers")]
+    public List<ApplicationUserModel> GetAllUsers()
+    {
+      List<ApplicationUserModel> output = new List<ApplicationUserModel>();
+
+      using (var context = new ApplicationDbContext())
+      {
+        var userStore = new UserStore<ApplicationUser>(context);
+        var userManager = new UserManager<ApplicationUser>(userStore);
+
+        var users = userManager.Users.ToList();
+        var roles = context.Roles.ToList();
+
+        foreach (var user in users)
+        {
+          ApplicationUserModel u = new ApplicationUserModel()
+          {
+            Id = user.Id,
+            Email = user.Email
+          };
+
+          foreach (var r in user.Roles)
+          {
+            u.Roles.Add(r.RoleId, roles.Where(x => x.Id == r.RoleId).First().Name);
+          }
+
+          output.Add(u);
+        }
+      }
+      return output;
+    }
+
+    //LinQ
+    //var result =
+    //                (from user in users
+    //                 select new ApplicationUserModel
+    //                 {
+    //                   Id = user.Id,
+    //                   Email = user.Email,
+    //                   Roles =
+    //                         (from userRole in user.Roles
+    //                          join role in roles on userRole.RoleId equals role.Id
+    //                          select new { userRole.RoleId, role.Name }).ToDictionary(o => o.RoleId, o => o.Name)
+    //                 }).ToList();
   }
 }
